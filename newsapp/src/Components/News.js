6056ab2740cc09b525d300552c17a57d32dde09f
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2022-07-07 01:31:58 +0500

    news cards added

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -4,15 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a News Component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        
+      <div className="container my-3">
+        <h1 className='text-center my-3'>Featured Headlines</h1>
+        <div className="row">
+            <div className='col-md-4'>
+                <NewsItem/>
+            </div>
+            <div className='col-md-4'>
+                <NewsItem/>
+            </div>
+            <div className='col-md-4'>
+                <NewsItem/>
+            </div>
+        </div>
       </div>
       
     )
--- newsapp/src/Components/NewsItem.js
+++ newsapp/src/Components/NewsItem.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
     return (
-      <div>This is a News item</div>
-    )
+      <div>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://s.yimg.com/os/creatr-uploaded-images/2022-01/353598f0-7765-11ec-bbbf-9f0ea07ea914" className="card-img-top" alt="..." />
+          <div className="card-body text-center">
+            <h5 className="card-title">Headline</h5>
+            <p className="card-text">
+              News Desc
+            </p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
